8a1a828c39253348963621336916c96cba891c1c
Jaqueline Fernandes <dhelly@gmail.com>
2020-11-04 16:24:34 -0300

    create test file

---
+++ test/index.php
@@ -0,0 +1,18 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Notification\Email;
+
+$email = new Email(
+    2,
+    "mail.host.com",
+    "your@email.com",
+    "your-pass",
+    "smtp secure (tls/ssl)",
+    "port (587)",
+    "from@email.com",
+    "From Name"
+);
+
+$email->sendEmail("SUbject", "Content", "reply@email.com", "Replay Name", "address@email.com", "Address Name");
